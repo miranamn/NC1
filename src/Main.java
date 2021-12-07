b76e7a3c3d31764f1d781cd072329ad198b7d781
Pavel Pecherskih <pecherskih@netcracker.com>
2021-12-07 20:25:59 +0400

    'OLOLO'

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,7 @@ public class Main {
         View view = new ConsoleView(controller);
         Loader list = new TrackListLoader(view);
         controller.setList(list);
+        
         view.init();
     }
 }
